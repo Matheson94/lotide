e05a9017b3cb3ceb8201b91786e5a8297658ac50
Matheson94 <mathesonchisholm@hotmail.com>
2022-01-11 22:17:06 +0000

    added eqArrays

---
+++ eqArrays.js
@@ -0,0 +1,32 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`⛔️⛔️⛔️Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+function eqArrays(arr1, arr2) {
+  let output;
+	if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+		return "Not An Array";
+	}
+  if (arr1.length !== arr2.length) return false;
+  for (var i = 0, length = arr1.length; i < length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
+assertEqual(eqArrays(["1", "2", "3"], "hello"), false);
