3677e6f8909837275aa168e60507d878cbf22aa9
Matheson94 <mathesonchisholm@hotmail.com>
2022-01-12 00:14:45 +0000

    W1D2 final Lotide Commit (hopefully)

--- without.js
+++ without.js
@@ -1,8 +1,7 @@
-
 function eqArrays(arr1, arr2) {
-	if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
-		return "Not An Array";
-	}
+  if (!Array.isArray(arr1) || !Array.isArray(arr2)) {
+    return "Not An Array";
+  }
   if (arr1.length !== arr2.length) return false;
   for (var i = 0, length = arr1.length; i < length; i++) {
     if (arr1[i] !== arr2[i]) {
@@ -14,10 +13,16 @@ function eqArrays(arr1, arr2) {
 
 
 const assertEqualArrays = (arr1, arr2) => {
-  if (eqArrays(arr1, arr2)) { 
+  if (eqArrays(arr1, arr2)) {
     console.log("Equal");
   } else {
     console.log("False");
   }
 }
+
+const without = function(arr1, arr2) {
+	let newArr = arr1.filter(x => !arr2.includes(x));
+	return newArr;
+}
+
 
