7c9cee7bcb78361ce1059a47e3f1af892ae9c89f
Matheson94 <mathesonchisholm@hotmail.com>
2022-01-17 22:08:15 +0000

    Final Lotide Push

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,6 +1,6 @@
 const assertEqual = require("../assertEqual");
 
 
-assertEqual("Lighthouse Labs", "LHL");
-assertEqual(1, 1);
-assertEqual("hello", "hello");
+// assertEqual("Lighthouse Labs", "LHL");
+// assertEqual(1, 1);
+// assertEqual("hello", "hello");
